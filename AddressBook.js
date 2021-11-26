8b9c80b52487dd68679ba1416ac7047fe9e23104
rahul yadav <ry219613@gmail.com>
2021-11-27 01:50:30 +0530

    create address book array

--- AddressBook.js
+++ AddressBook.js
@@ -1,20 +1,28 @@
 //importing another class file to the class file
-const PersonInformation = require("./PersonInformation.js"); 
+const PersonInformation = require("./PersonInformation.js");
 const prompt = require('prompt-sync')();
 
+class AddressBook {
+    contactList = new Array();
+    insert(contactList) {
+        let personInfo = new PersonInformation();
+        try {
+            personInfo.firstName = prompt("Enter First Name : ");
+            personInfo.lastName = prompt("Enter Last Name : ");
+            personInfo.address = prompt("Enter Address : ");
+            personInfo.city = prompt("Enter City : ");
+            personInfo.state = prompt("Enter State : ");
+            personInfo.zip = prompt("Enter Zip : ");
+            personInfo.phoneNumber = prompt("Enter Phone Number : ");
+            personInfo.email = prompt("Enter Email : ");
+            contactList.push(personInfo);
 
-let personInfo = new PersonInformation();
-try{
-    personInfo.firstName = prompt("Enter First Name : "); 
-    personInfo.lastName = prompt("Enter Last Name : "); 
-    personInfo.address = prompt("Enter Address : "); 
-    personInfo.city = prompt("Enter City : "); 
-    personInfo.state = prompt("Enter State : "); 
-    personInfo.zip = prompt("Enter Zip : "); 
-    personInfo.phoneNumber = prompt("Enter Phone Number : "); 
-    personInfo.email = prompt("Enter Email : "); 
-    console.log(personInfo.toString());
+            return contactList;
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
 }
-catch (e) {
-    console.error(e);
-}
+
+module.exports = AddressBook;
